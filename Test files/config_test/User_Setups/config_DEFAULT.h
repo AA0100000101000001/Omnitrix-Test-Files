0ab67e99367806df426e5d35c77040323a8914a0
AA0100000101000001 <gracedgold26@gmail.com>
2024-02-05 21:14:50 +0200

    Added Pin Configuration

--- Test files/config_test/User_Setups/config_DEFAULT.h
+++ Test files/config_test/User_Setups/config_DEFAULT.h
@@ -1,8 +1,11 @@
 
+/*	Edit this file to make new configuration files for other boards
+*/
+
 //Uncomment for CST816S touch driver and GC9A01 lcd display
-#define ESP32_S3_ROUND_DISPLAY_1_28
+#define ESP32_S3_TOUCH_LCD_1_28
 //Uncomment for no touch GC9A01 display
-#define OMNITRIX_CUSTOM_GC9A01
+#define ESP32_S2_PINOUT
 
 //Uncomment for buttons
 #define BUTTONS_ENABLED
@@ -12,3 +15,35 @@
 
 //Uncomment for RGB LEDS
 #define RGB_LEDS_ENABLED
+
+
+//Define pins for ESP32-S2 board
+//Define button pins
+#if defined ESP32_S2_PINOUT && defined BUTTONS_ENABLED
+
+	#define START_BUTTON_PIN 5
+	#define RIGHT_BUTTON_PIN 1
+	#define LEFT_BUTTON_PIN 4
+	#define SELECT_BUTTON_PIN 3
+
+#endif
+//Define RGB LED pins
+#if defined ESP32_S2_PINOUT && defined RGB_LEDS_ENABLED
+
+	#define RGB_LED_R 6
+	#define RGB_LED_G 7
+	#define RGB_LED_B 8
+
+#endif
+
+
+//Define pins for ESP32-S3 Touch LCD 1.28 board
+//Define button pins
+#if defined ESP32_S3_TOUCH_LCD_1_28 && defined BUTTONS_ENABLED
+	
+	#define START_BUTTON_PIN 15
+	#define RIGHT_BUTTON_PIN 16
+	#define LEFT_BUTTON_PIN 17
+	#define SELECT_BUTTON_PIN 18
+
+#endif
---
+++ Test files/config_test/User_Setups/config_ESP32_S3_TOUCH_LCD_1_28.h
@@ -0,0 +1,46 @@
+
+//Uncomment for CST816S touch driver and GC9A01 lcd display
+#define ESP32_S3_TOUCH_LCD_1_28
+//Uncomment for no touch GC9A01 display
+//#define ESP32_S2_PINOUT
+
+//Uncomment for buttons
+#define BUTTONS_ENABLED
+
+//Uncomment for sound
+//#define SOUND_ENABLED
+
+//Uncomment for RGB LEDS
+//#define RGB_LEDS_ENABLED
+
+
+//Define pins for ESP32-S2 board
+//Define button pins
+#if defined ESP32_S2_PINOUT && defined BUTTONS_ENABLED
+
+	#define START_BUTTON_PIN 5
+	#define RIGHT_BUTTON_PIN 1
+	#define LEFT_BUTTON_PIN 4
+	#define SELECT_BUTTON_PIN 3
+
+#endif
+//Define RGB LED pins
+#if defined ESP32_S2_PINOUT && defined RGB_LEDS_ENABLED
+
+	#define RGB_LED_R 6
+	#define RGB_LED_G 7
+	#define RGB_LED_B 8
+
+#endif
+
+
+//Define pins for ESP32-S3 Touch LCD 1.28 board
+//Define button pins
+#if defined ESP32_S3_TOUCH_LCD_1_28 && defined BUTTONS_ENABLED
+	
+	#define START_BUTTON_PIN 15
+	#define RIGHT_BUTTON_PIN 16
+	#define LEFT_BUTTON_PIN 17
+	#define SELECT_BUTTON_PIN 18
+
+#endif
