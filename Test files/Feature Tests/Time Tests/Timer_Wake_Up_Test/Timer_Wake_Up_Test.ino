703cfea5d4457160ef541dd45a9202f464dd0582
AA0100000101000001 <gracedgold26@gmail.com>
2024-04-05 21:03:35 +0300

    Revert "Merge branch 'Testing'"
    This reverts commit 4852e21e45a35c28635af4232be127402863bcdd, reversing
    changes made to cf148ba26ca478e3a22955d6a7ab7124fbd553aa.

--- Test files/Feature Tests/Time Tests/Timer_Wake_Up_Test/Timer_Wake_Up_Test.ino
+++ Test files/Feature Tests/Time Tests/Timer_Wake_Up_Test/Timer_Wake_Up_Test.ino
@@ -266,9 +266,6 @@ void get_wakeup_reason() {
         }
         break;
       }
-    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
-    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
     default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
   }
 
