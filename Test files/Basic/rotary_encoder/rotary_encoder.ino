f3793c28c1b625e49ba793ea10bdc96a1eee6dac
AA0100000101000001 <gracedgold26@gmail.com>
2024-04-26 20:46:40 +0300

    Pin Name Change

--- Test files/Basic/rotary_encoder/rotary_encoder.ino
+++ Test files/Basic/rotary_encoder/rotary_encoder.ino
@@ -4,22 +4,21 @@ Tested with:
 ESP32-S2-DEV 
 */
 
-int anienNo = 0;
-int rightState = 0;
-int leftState = 0;
+#define ROTARY_LEFT_PIN 4
+#define ROTARY_RIGHT_PIN 1
+#define ROTARY_BUTTON_PIN 3
+
+bool rightState = 0;
+bool leftState = 0;
+bool buttonState = 0;
+
 int encoderPos = 0;
-int buttonState = 0;
-
-
-int A = 4;
-int B = 1;
-int S = 3;
 
 void setup() {
   
-  pinMode(A, INPUT);
-  pinMode(B, INPUT);
-  pinMode(S, INPUT_PULLUP);
+  pinMode(ROTARY_LEFT_PIN, INPUT);
+  pinMode(ROTARY_RIGHT_PIN, INPUT);
+  pinMode(ROTARY_BUTTON_PIN, INPUT_PULLUP);
 
   Serial.begin(115200);
 
@@ -28,21 +27,21 @@ void setup() {
 void loop() {
 
 
-  rightState = digitalRead(A);
+  rightState = digitalRead(ROTARY_LEFT_PIN);
 
   if (rightState == LOW) {
     Serial.println("Right");
     delay(200);
   }
   
-  leftState = digitalRead(B);
+  leftState = digitalRead(ROTARY_RIGHT_PIN);
 
   if (leftState == LOW) {
     Serial.println("Left");
     delay(200);
   }
   
-  buttonState = digitalRead(S);
+  buttonState = digitalRead(ROTARY_BUTTON_PIN);
   if (buttonState == HIGH) {
     delay(200);
     Serial.println("Select Button pressed");
