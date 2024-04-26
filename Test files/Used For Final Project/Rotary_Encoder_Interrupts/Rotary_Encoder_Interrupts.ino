1be17690e1658bc15cd7f49c54650f1269e0e7a5
AA0100000101000001 <gracedgold26@gmail.com>
2024-04-27 01:17:42 +0300

    Update Projects Used For Final Testing

---
+++ Test files/Used For Final Project/Rotary_Encoder_Interrupts/Rotary_Encoder_Interrupts.ino
@@ -0,0 +1,62 @@
+
+#define ROTARY_LEFT_PIN 4
+#define ROTARY_RIGHT_PIN 1
+
+bool rightState = 0; //State of right
+bool leftState = 0; //State of left
+
+void setup() {
+  pinMode(ROTARY_LEFT_PIN, INPUT_PULLUP); // Using internal pullup resistor
+  pinMode(ROTARY_RIGHT_PIN, INPUT_PULLUP);
+
+  attachInterrupt(ROTARY_LEFT_PIN, rotEncoder, CHANGE); // Need to detect both rising or falling signal
+  attachInterrupt(ROTARY_RIGHT_PIN, rotEncoder, CHANGE);
+
+  Serial.begin (115200);
+}
+
+void rotEncoder() {
+
+  static uint8_t old_AB = 3;  // Lookup table index
+  static int8_t encval = 0;   // Encoder value  
+  static const int8_t enc_states[]  = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0}; // Lookup table
+  static unsigned long lastInterruptTime = 0;
+  unsigned long interruptTime = millis();
+
+  old_AB <<=2;  // Remember previous state 
+
+  if (digitalRead(ROTARY_LEFT_PIN)) old_AB |= 0x02; // Add current state of pin A
+  if (digitalRead(ROTARY_RIGHT_PIN)) old_AB |= 0x01; // Add current state of pin B
+   
+  encval += enc_states[( old_AB & 0x0f )];
+ 
+  // Update counter if encoder has rotated a full indent, that is at least 4 steps
+  if( encval > 3 ) {        // Four steps forward
+    if (interruptTime - lastInterruptTime > 80) {
+      rightState = true; //Right
+    }
+    encval = 0;
+    lastInterruptTime = millis(); 
+  }
+  else if( encval < -3 ) {  // Four steps backwards
+   if (interruptTime - lastInterruptTime > 80) {
+      leftState = true; //Left
+    }
+    encval = 0;
+    lastInterruptTime = millis(); 
+  }
+}
+
+void loop() {
+
+  if (rightState) {
+    Serial.printf("Right\n");
+    rightState = false;
+
+  }
+  if (leftState) {
+    Serial.printf("Left\n");
+    leftState = false;
+
+  }
+}
