4f73ea2900aa5b2a15104b0afd1e6a0babb1d22f
AA0100000101000001 <gracedgold26@gmail.com>
2024-05-02 17:56:27 +0300

    Convert Return Point To Integer

--- Test files/Basic/Find_Intersecting_Point_Of_Two_Lines/Find_Intersecting_Point_Of_Two_Lines.ino
+++ Test files/Basic/Find_Intersecting_Point_Of_Two_Lines/Find_Intersecting_Point_Of_Two_Lines.ino
@@ -31,9 +31,9 @@ void loop() {
   //1*x + 1*y = 240
 
   //Test Omnitrix animation line equations  
-  float Ax1 = -6.0, Ay1 = 0.0, Ax2 = 74.0, Ay2 = 120.0; //first line points
-  float Bx1 = 246.0, By1 = 0.0, Bx2 = 166.0, By2 = 120.0; //second line points
-  float x = 0, y = 0; //x,y result
+  int Ax1 = -6, Ay1 = 0, Ax2 = 74, Ay2 = 120; //first line points
+  int Bx1 = 246, By1 = 0, Bx2 = 166, By2 = 120; //second line points
+  int x = 0, y = 0; //x,y result
 
   //Lines are always intersecting because they have opposite slopes 
   //but the function will be called after x > 120 where the intersecting matters the most
@@ -63,22 +63,24 @@ void loop() {
 
 //void getCommonPoint(float sA1, float sB1, float sC1, float sA2, float sB2, float sC2, float* x, float* y) {
 //Find common point of lines A[(Ax1, Ay1),(Ax2, Ay2)] and B[(Bx1, By1),(Bx2, By2)]
-float getCommonPoint(float Ax1, float Ay1, float Ax2, float Ay2, float Bx1, float By1, float Bx2, float By2, float* x, float* y) {
+float getCommonPoint(int Ax1, int Ay1, int Ax2, int Ay2, int Bx1, int By1, int Bx2, int By2, int* x, int* y) {
 
-  float dx, dy, Aa, Ba, Ab, Bb;
+  int dx, dy;
+  float Aa, Ba, Ab, Bb;
+  float fx, fy;
 
   //Find A's equation
   dx = Ax2 - Ax1;
   dy = Ay2 - Ay1;
-  Aa = dy / dx; //slope a
-  Ab = Ay1 - Aa*Ax1; //b = y1 - ax1
+  Aa = (float) dy / dx; //slope a
+  Ab = (float) Ay1 - Aa*Ax1; //b = y1 - ax1
   //y = Aa*x + Ab
   
   //Find B's equation
   dx = Bx2 - Bx1;
   dy = By2 - By1;
-  Ba = dy / dx; //slope a
-  Bb = By1 - Ba*Bx1; //b = y1 - ax1
+  Ba = (float) dy / dx; //slope a
+  Bb = (float) By1 - Ba*Bx1; //b = y1 - ax1
   //y = Ba*x + Bb
 /*
   Serial.print("Aa = ");
@@ -108,8 +110,11 @@ float getCommonPoint(float Ax1, float Ay1, float Ax2, float Ay2, float Bx1, floa
 
   //*x = (sB2 * sC1 - sB1 * sC2) / delta;
   //*y = (sA1 * sC2 - sA2 * sC1) / delta;
-  *x = -((-1) * Ab - (-1) * Bb) / delta;
-  *y = -(Aa * Bb - Ba * Ab) / delta;
+  fx = -((-1) * Ab - (-1) * Bb) / delta;
+  fy = -(Aa * Bb - Ba * Ab) / delta;
+  //Round to integer
+  *x = (int) (fx < 0 ? (fx - 0.5) : (fx + 0.5));
+  *y =(int) (fy < 0 ? (fy - 0.5) : (fy + 0.5));
   
   return delta;
 
